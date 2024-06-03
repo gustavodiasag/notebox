e0255891919e74b8f61b899d78849b41f07c41d5
Gustavo Dias de Aguiar <gustavodias.aguiar1@gmail.com>
2024-06-03 20:09:41 -0300

    feat: add MySQL database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,48 +1,68 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gustavodiasag/notebox/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	notes    *models.NoteModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/notebox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		notes:    &models.NoteModel{DB: db},
 	}
-
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/note/view", app.noteView)
-	mux.HandleFunc("/note/create", app.noteCreate)
 
 	// Define a new http.Server struct so that the server now uses the custom error
 	// logger instead of the standard one in the event of any problems.
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// No connections are actually created from `Open` as it establishes them lazily.
+	// `Ping` is used to create a connection and check for any errors.
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
