52b053b8cef890645d80c2a455682d60dac1988f
Gustavo Dias de Aguiar <gustavodias.aguiar1@gmail.com>
2024-06-03 20:11:38 -0300

    refac: separate the definition of routes in the app

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/note/view", app.noteView)
+	mux.HandleFunc("/note/create", app.noteCreate)
+
+	return mux
+}
