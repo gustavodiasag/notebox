2266f5b866fd093fd456fc78b8c0dbb7f17c8e02
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-06-27 17:57:13 -0300

    feat: add middleware for request logging

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -15,3 +15,12 @@ func secureHeaders(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// Defined against `application` to access dependencies.
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,5 +12,5 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/note/view", app.noteView)
 	mux.HandleFunc("/note/create", app.noteCreate)
 
-	return secureHeaders(mux)
+	return app.logRequest(secureHeaders(mux))
 }
