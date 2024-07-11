65a1534054826de18a68bd16442aa95d340be64c
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-11 15:47:39 -0300

    feat: add user logout endpoint

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -140,7 +140,17 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "foo")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
