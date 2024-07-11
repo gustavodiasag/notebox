d67279863a0032c1f5c529fa833488df4d1452b4
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-11 15:34:51 -0300

    refac(errors): add field-unrelated errors for forms

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -8,11 +8,16 @@ import (
 
 // Contains a map of validation errors for form fields.
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
