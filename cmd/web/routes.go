364e7f10766c4a8a187b85992d902ce932a35a77
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-12 09:14:33 -0300

    refac: enforce authentication for certain routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,19 +16,23 @@ func (app *application) routes() http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+
 	// Middleware chain containing the middleware specific to the dynamic application routes.
 	dyn := alice.New(app.sessionManager.LoadAndSave)
 
 	router.Handler(http.MethodGet, "/", dyn.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/note/view/:id", dyn.ThenFunc(app.noteView))
-	router.Handler(http.MethodGet, "/note/create", dyn.ThenFunc(app.noteCreate))
-	router.Handler(http.MethodPost, "/note/create", dyn.ThenFunc(app.noteCreatePost))
-
 	router.Handler(http.MethodGet, "/user/signup", dyn.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dyn.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dyn.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dyn.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", dyn.ThenFunc(app.userLogoutPost))
+
+	// Authenticated-only routes.
+	protected := dyn.Append(app.requireAuthentication)
+
+	router.Handler(http.MethodGet, "/note/create", protected.ThenFunc(app.noteCreate))
+	router.Handler(http.MethodPost, "/note/create", protected.ThenFunc(app.noteCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	// Middleware chain containing the standard middleware for the application.
 	std := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
