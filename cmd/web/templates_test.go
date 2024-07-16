770c6363119af6cb48dca1be63be377f6a2334c9
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-16 13:35:59 -0300

    test(template): refactor tests into a trable-driven suite

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -3,13 +3,38 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/gustavodiasag/notebox/internal/assert"
 )
 
 func TestHumanDate(t *testing.T) {
-	td := time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC)
-	fd := fmtDate(td)
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.UTC),
+			want: "17 Mar, 2022",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2022, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar, 2022",
+		},
+	}
 
-	if fd != "17 Mar, 2022" {
-		t.Errorf("got %q, want %q", fd, "17 Mar, 2022")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fd := fmtDate(tt.tm)
+
+			assert.Equal(t, fd, tt.want)
+		})
 	}
 }
