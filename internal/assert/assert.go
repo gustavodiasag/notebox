c173ea0f7ac9273472a059aad38b1c6d2bb26cdb
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-19 11:39:28 -0300

    Revert "feat(utils): add test utility for finding substring in response body"
    This reverts commit 69e04fbb82fd3901ad8c8f62c56d1b299c2a6f87.

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,22 +1,11 @@
 package assert
 
-import (
-	"strings"
-	"testing"
-)
+import "testing"
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("got %v, want %v", actual, expected)
-	}
-}
-
-func StringContains(t *testing.T, actual, substr string) {
-	t.Helper()
-
-	if !strings.Contains(actual, substr) {
-		t.Errorf("got: %q; expected to contain: %q", actual, substr)
 	}
 }
