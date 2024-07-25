a496666e232cdf3fbbf92941617995de154e37b8
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-25 10:40:43 -0300

    test(handlers): add suite for user signupendpoint

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -76,3 +76,15 @@ func TestNoteView(t *testing.T) {
 		})
 	}
 }
+
+func TestUserSignup(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	_, _, body := ts.get(t, "/user/signup")
+	csrfToken := extractCSRFToken(t, body)
+
+	t.Logf("CSRF token is: %q", csrfToken)
+}
