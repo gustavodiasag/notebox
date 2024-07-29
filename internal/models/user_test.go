9901d846e8fce839f170e58f612954acd71f17e9
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-29 18:03:41 -0300

    test(integration): add suite for user existense on database

---
+++ internal/models/user_test.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/gustavodiasag/notebox/internal/assert"
+)
+
+func TestUserModelExists(t *testing.T) {
+	tests := []struct {
+		name   string
+		userID int
+		want   bool
+	}{
+		{
+			name:   "Valid ID",
+			userID: 1,
+			want:   true,
+		},
+		{
+			name:   "Zero ID",
+			userID: 0,
+			want:   false,
+		},
+		{
+			name:   "Non-Existent ID",
+			userID: 2,
+			want:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := newTestDB(t)
+
+			m := UserModel{db}
+
+			exists, err := m.Exists(tt.userID)
+
+			assert.Equal(t, exists, tt.want)
+			assert.NilError(t, err)
+		})
+	}
+}
