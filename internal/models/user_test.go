0c6e128db4f9981d00a5a6e7e9c9c4faae8b067a
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-29 18:12:39 -0300

    refac: skip integration tests when needed

--- internal/models/user_test.go
+++ internal/models/user_test.go
@@ -7,6 +7,10 @@ import (
 )
 
 func TestUserModelExists(t *testing.T) {
+	if testing.Short() {
+		t.Skip("models: skipping integration test")
+	}
+
 	tests := []struct {
 		name   string
 		userID int
