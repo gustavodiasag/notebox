20edc44d914dc852af50d76b451eb4ac6b92a499
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-07-29 17:43:33 -0300

    feat(assert): add helper for nil error assertion

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -9,7 +9,7 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
-		t.Errorf("got %v, want %v", actual, expected)
+		t.Errorf("got: %v, want: %v", actual, expected)
 	}
 }
 
@@ -18,5 +18,13 @@ func StringContains(t *testing.T, actual, substr string) {
 
 	if !strings.Contains(actual, substr) {
 		t.Errorf("got: %q; expected to contain: %q", actual, substr)
+	}
+}
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got %v, expected: nil", actual)
 	}
 }
